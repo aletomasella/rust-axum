d3cf306df4ccdd0481967658cda45c88c320bda1
aletomasella <aletomasella09@gmail.com>
2022-10-12 19:37:17 -0300

    Adding config file

--- src/endpoints/endpoints.rs
+++ src/endpoints/endpoints.rs
@@ -11,6 +11,11 @@ async fn version() -> impl Responder {
     HttpResponse::Ok().body("0.1.0")
 }
 
+#[get("/hello/{name}")]
+async fn hello(name : web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello {}!", name))
+}
+
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 
-use actix_web::{ App, HttpServer};
+use actix_web::{ App, HttpServer, web};
 use rust_actix_api::endpoints::endpoints::init;
-
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .configure(init)
+            .service(web::scope("/api").configure(init))
     })
-    .bind(("127.0.0.1", 3001))?
+    .bind(("127.0.0.1", 3002))?
     .run()
     .await
 }
