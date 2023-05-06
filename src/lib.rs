a67aadf3b5bcacc3a1fb09d7b6fc3111e86790c6
Alejandro Tomasella <aletomasella09@gmail.com>
2023-05-06 16:20:37 -0300

    feat: Removing old API

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod todolist;
-pub mod config;
-pub mod data;
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-
-use actix_web::{ App, HttpServer, web};
-use rust_actix_api::todolist::controllers::init_todolist;
-use rust_actix_api::config::{read_config};
-use rust_actix_api::data::{AppState};
-use std::sync::Mutex;
-
-
-
-
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-let config = read_config();
-
-let app_data = web::Data::new(AppState {
-    todolist_entries: Mutex::new(vec![]),
-});
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(app_data.clone())
-            .service(web::scope("/todolist").configure(init_todolist))
-    })
-    .bind((config.host, config.port))? // <- bind address
-    .run()
-    .await
+fn main () {
+    println!("Hello, world!");
 }
